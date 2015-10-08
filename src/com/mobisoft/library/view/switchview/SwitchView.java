916f8efec169c3ddae9ad22c0b67317b42f04b33
canvaser <18721607438@163.com>
2015-10-08 14:46:12 +0800

    add switchbutton

--- src/com/mobisoft/library/view/switchview/SwitchView.java
+++ src/com/mobisoft/library/view/switchview/SwitchView.java
@@ -7,12 +7,13 @@ import android.graphics.Paint;
 import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.View.OnClickListener;
 
 /**
  * 开关按钮
  * Created by canvaser on 2015/9/29.
  */
-public class SwitchView extends View{
+public class SwitchView extends View implements OnClickListener{
 
 	/**宽度*/
 	private int sv_width=0;
@@ -20,22 +21,56 @@ public class SwitchView extends View{
 	private int sv_height=0;
 	/**画笔*/
 	private Paint sv_paint=new Paint();
+	/**圆半径*/
+	private int sv_circle_radious=0;
+	/**边距*/
+	private int sv_border=0;
+	/**开关状态*/
+	private boolean opened=false;
 	public SwitchView(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		setOnClickListener(this);
 	}
 
 	@Override
 	protected void onDraw(Canvas canvas) {
-		sv_paint.setColor(Color.RED);
-		RectF rectF=new RectF();
-		rectF.left=0;
-		rectF.right=sv_width;
-		rectF.bottom=sv_height;
-		rectF.top=0;
-		canvas.drawOval(rectF,sv_paint);
-		//canvas.drawOval(0,0,sv_width,sv_height,sv_paint);
-		//canvas.drawOval(0,0,sv_width,sv_height,sv_paint);
-		//canvas.drawRoundRect(0,0,sv_width,sv_height,sv_width/5,sv_height/5,sv_paint);
+		if(opened){
+//			sv_paint.setColor(Color.BLACK);
+//			sv_paint.setAntiAlias(true);
+//			RectF rectF=new RectF();
+//			rectF.left=0;
+//			rectF.right=sv_width;
+//			rectF.bottom=sv_height;
+//			rectF.top=0;
+//			canvas.drawRoundRect(rectF,sv_height/2,sv_height/2, sv_paint);
+//			sv_paint.setColor(Color.WHITE);
+//			RectF rectF2=new RectF();
+//			rectF2.left=sv_height/20f;
+//			rectF2.right=sv_width-sv_height/20f;
+//			rectF2.bottom=sv_height-sv_height/20f;
+//			rectF2.top=sv_height/20f;
+//			canvas.drawRoundRect(rectF2,sv_height*9/20f,sv_height*9/20f, sv_paint);
+//			sv_paint.setColor(Color.RED);
+//			canvas.drawCircle(sv_width-sv_circle_radious-sv_border,sv_height/2f, sv_circle_radious, sv_paint);
+		}else{
+//			sv_paint.setColor(Color.BLACK);
+//			sv_paint.setAntiAlias(true);
+//			RectF rectF=new RectF();
+//			rectF.left=0;
+//			rectF.right=sv_width;
+//			rectF.bottom=sv_height;
+//			rectF.top=0;
+//			canvas.drawRoundRect(rectF,sv_height/2,sv_height/2, sv_paint);
+//			sv_paint.setColor(Color.WHITE);
+//			RectF rectF2=new RectF();
+//			rectF2.left=sv_height/20f;
+//			rectF2.right=sv_width-sv_height/20f;
+//			rectF2.bottom=sv_height-sv_height/20f;
+//			rectF2.top=sv_height/20f;
+//			canvas.drawRoundRect(rectF2,sv_height*9/20f,sv_height*9/20f, sv_paint);
+//			sv_paint.setColor(Color.RED);
+//			canvas.drawCircle(sv_border+sv_circle_radious,sv_height/2f, sv_circle_radious, sv_paint);
+		}
 	}
 
 
@@ -50,5 +85,13 @@ public class SwitchView extends View{
 		super.onLayout(changed, left, top, right, bottom);
 		sv_width=getWidth();
 		sv_height=getHeight();
+		sv_circle_radious=sv_height*5/12;
+		sv_border=sv_height/12;
+	}
+
+	@Override
+	public void onClick(View v) {
+		opened=!opened;
+		postInvalidate();
 	}
 }
